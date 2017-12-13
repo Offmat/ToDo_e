33038d2ef303318d32f76453d357e3683cbbb2ba
Mateusz <lesniak.mat@gmail.com>
2017-12-13 21:50:10 +0100

    Update and delete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,22 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params) #wypróbować errory przy walidacji dla .attributes i dla .update
+    redirect_to @task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.delete
+    redirect_to tasks_path
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :body)
